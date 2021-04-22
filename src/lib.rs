0f85fd4453d0413c8595bf973b330aac0cb77799
nicohman <nicohickman@protonmail.com>
2021-04-22 09:36:24 -0700

    Expose State in prelude

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub use crate::manager::Manager;
 pub use libsignal_protocol::Context as ProtocolContext;
 
 pub mod prelude {
+    pub use crate::manager::State;
     pub use libsignal_protocol::{crypto::DefaultCrypto, Context};
     pub use libsignal_service::content::{
         self, sync_message, AttachmentPointer, ContentBody, DataMessage, GroupContextV2, Metadata,
