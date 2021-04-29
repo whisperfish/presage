b1eae6745f32c8968f0828fa97bb014cddeb85c9
boxdot <d@zerovolt.org>
2021-04-29 16:22:40 +0200

    Use a single ConfigStore param in Manager.
    The ConfigStore trait already requires all other trais being
    implmented, so there is no need to store them.
    
    This also makes the random generator parameter default.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,12 @@ mod manager;
 pub use crate::errors::Error;
 pub use crate::manager::{Manager, State};
 
+/// Re-export of `libsignal-service` crate
+pub use libsignal_service;
+
+/// Re-export of Signal protobufs
+pub use libsignal_service::proto;
+
 pub mod prelude {
     pub mod service {
         pub use libsignal_service::{
