0caea64868fbc36fdca81a7aca20af8a58042e3b
gferon <g@leirbag.net>
2022-06-02 19:31:55 +0000

    Deploying to gh-pages from  @ 021ed1d0adce318150b99c301f28b636d3ffb27e 🚀

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["presage"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"presage/enum.Error.html\" title=\"enum presage::Error\">Error</a>","synthetic":false,"types":["presage::errors::Error"]}];
+implementors["presage"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"presage/enum.Error.html\" title=\"enum presage::Error\">Error</a>","synthetic":false,"types":["presage::errors::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
