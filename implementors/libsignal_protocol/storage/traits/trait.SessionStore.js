af30b355a549341d271a974c1f2115d6a0ba2bd4
gferon <g@leirbag.net>
2022-07-12 16:16:07 +0000

    Deploying to gh-pages from  @ 91d1a3824d46688f377e5257c2c3c8f1b3a0583c 🚀

--- implementors/libsignal_protocol/storage/traits/trait.SessionStore.js
+++ implementors/libsignal_protocol/storage/traits/trait.SessionStore.js
@@ -1,3 +1,5 @@
 (function() {var implementors = {};
+implementors["libsignal_protocol"] = [];
+implementors["libsignal_service"] = [];
 implementors["presage"] = [{"text":"impl <a class=\"trait\" href=\"libsignal_protocol/storage/traits/trait.SessionStore.html\" title=\"trait libsignal_protocol::storage::traits::SessionStore\">SessionStore</a> for <a class=\"struct\" href=\"presage/struct.SledConfigStore.html\" title=\"struct presage::SledConfigStore\">SledConfigStore</a>","synthetic":false,"types":["presage::config::sled::SledConfigStore"]},{"text":"impl <a class=\"trait\" href=\"libsignal_protocol/storage/traits/trait.SessionStore.html\" title=\"trait libsignal_protocol::storage::traits::SessionStore\">SessionStore</a> for <a class=\"struct\" href=\"presage/struct.VolatileConfigStore.html\" title=\"struct presage::VolatileConfigStore\">VolatileConfigStore</a>","synthetic":false,"types":["presage::config::volatile::VolatileConfigStore"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
