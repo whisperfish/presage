whisperfish/presage

commit c5f23d4153f78726d104b2db49d5b21f7be658e0

Only save DataMessage and SyncMessage (sent) in store (#137)

src/manager.rs
@@ -809,13 +809,10 @@ impl<C: Store> Manager<C, Registered> {
                                     }
                                 }
 
-                                if let Ok(thread) = Thread::try_from(&content) {
-                                    // TODO: handle reactions here, we should update the original message?
-                                    if let Err(e) =
-                                        state.config_store.save_message(&thread, content.clone())
-                                    {
-                                        log::error!("Error saving message to store: {}", e);
-                                    }
+                                if let Err(e) =
+                                    save_message(&mut state.config_store, content.clone())
+                                {
+                                    log::error!("Error saving message to store: {}", e);
                                 }
 
                                 return Some((content, state));
@@ -859,7 +856,6 @@ impl<C: Store> Manager<C, Registered> {
             .await?;
 
         // save the message
-        let thread = Thread::Contact(recipient.uuid);
         let content = Content {
             metadata: Metadata {
                 sender: self.state.uuid.into(),
@@ -871,7 +867,7 @@ impl<C: Store> Manager<C, Registered> {
             body: content_body,
         };
 
-        self.config_store.save_message(&thread, content)?;
+        save_message(&mut self.config_store, content)?;
 
         Ok(())
     }
@@ -923,8 +919,8 @@ impl<C: Store> Manager<C, Registered> {
             },
             body: message.into(),
         };
-        let thread = Thread::try_from(&content)?;
-        self.config_store.save_message(&thread, content)?;
+
+        save_message(&mut self.config_store, content)?;
 
         Ok(())
     }
@@ -1093,4 +1089,27 @@ async fn upsert_group<C: Store>(
     }
 
     config_store.group(master_key_bytes)
+}
+
+fn save_message<C: Store>(config_store: &mut C, message: Content) -> Result<(), Error> {
+    let thread = Thread::try_from(&message)?;
+    // only save DataMessage and SynchronizeMessage (sent)
+    match message.body {
+        ContentBody::DataMessage(_)
+        | ContentBody::SynchronizeMessage(SyncMessage { sent: Some(_), .. })
+        | ContentBody::SynchronizeMessage(SyncMessage {
+            call_event: Some(_),
+            ..
+        }) => {
+            config_store.save_message(&thread, message)?;
+        }
+        ContentBody::SynchronizeMessage(_) => {
+            debug!("skipping saving sync message without interesting fields")
+        }
+        ContentBody::CallMessage(_) => debug!("skipping saving call message"),
+        ContentBody::ReceiptMessage(_) => debug!("skipping saving receipt message"),
+        ContentBody::TypingMessage(_) => debug!("skipping saving typing message"),
+    }
+
+    Ok(())
 }

src/store/mod.rs
@@ -159,7 +159,6 @@ pub trait MessageStore {
     fn clear_messages(&mut self) -> Result<(), Error>;
 
     /// Save a message in a [Thread] identified by a timestamp.
-    /// TODO: deriving the thread happens from the content, so we can also ditch the first parameter
     fn save_message(&mut self, thread: &Thread, message: Content) -> Result<(), Error>;
 
     /// Delete a single message, identified by its received timestamp from a thread.

src/store/sled.rs
@@ -782,7 +782,7 @@ impl MessageStore for SledStore {
             message.metadata.timestamp,
         );
 
-        let tree = self.messages_thread_tree_name(thread);
+        let tree = self.messages_thread_tree_name(&thread);
         let key = message.metadata.timestamp.to_be_bytes();
 
         let proto: ContentProto = message.into();

commit 3abf386f988822a611b110ef2b8b542586553ec4

Fix storing outgoing messages
Commit c5f23d4 broke storing outgoing messages by not setting the
message thread and instead calculating it from the message, which can
not differentiate between incoming and outgoing messages. This sets the
thread for outgoing messages like done before.

This commit also stores call messages as I think these should also be
shown in the chat-history and should therefore be stored. This is
debatable though as I have no idea how Signal is handling that.

src/manager.rs
@@ -878,7 +878,8 @@ impl<C: Store> Manager<C, Registered> {
             body: content_body,
         };
 
-        save_message(&mut self.config_store, content)?;
+        let thread = Thread::Contact(recipient.uuid);
+        save_message_with_thread(&mut self.config_store, content, thread)?;
 
         Ok(())
     }
@@ -1102,8 +1103,11 @@ async fn upsert_group<C: Store>(
     config_store.group(master_key_bytes)
 }
 
-fn save_message<C: Store>(config_store: &mut C, message: Content) -> Result<(), Error> {
-    let thread = Thread::try_from(&message)?;
+fn save_message_with_thread<C: Store>(
+    config_store: &mut C,
+    message: Content,
+    thread: Thread,
+) -> Result<(), Error> {
     // only save DataMessage and SynchronizeMessage (sent)
     match message.body {
         ContentBody::DataMessage(_)
@@ -1117,10 +1121,15 @@ fn save_message<C: Store>(config_store: &mut C, message: Content) -> Result<(), 
         ContentBody::SynchronizeMessage(_) => {
             debug!("skipping saving sync message without interesting fields")
         }
-        ContentBody::CallMessage(_) => debug!("skipping saving call message"),
+        ContentBody::CallMessage(_) => config_store.save_message(&thread, message)?,
         ContentBody::ReceiptMessage(_) => debug!("skipping saving receipt message"),
         ContentBody::TypingMessage(_) => debug!("skipping saving typing message"),
     }
 
     Ok(())
+}
+
+fn save_message<C: Store>(config_store: &mut C, message: Content) -> Result<(), Error> {
+    let thread = Thread::try_from(&message)?;
+    save_message_with_thread(config_store, message, thread)
 }