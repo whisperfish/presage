ab6990f36564770a1fd95052641e858ee4d5bc2b
Gabriel Féron <g@leirbag.net>
2023-11-16 21:14:11 +0100

    Fix compilation

--- presage/src/manager/linking.rs
+++ presage/src/manager/linking.rs
@@ -28,12 +28,12 @@ impl<S: Store> Manager<S, Linking> {
     /// use futures::{channel::oneshot, future, StreamExt};
     /// use presage::libsignal_service::configuration::SignalServers;
     /// use presage::Manager;
-    /// use presage_store_sled::{MigrationConflictStrategy, SledStore};
+    /// use presage_store_sled::{MigrationConflictStrategy, OnNewIdentity, SledStore};
     ///
     /// #[tokio::main]
     /// async fn main() -> Result<(), Box<dyn std::error::Error>> {
     ///     let store =
-    ///         SledStore::open("/tmp/presage-example", MigrationConflictStrategy::Drop)?;
+    ///         SledStore::open("/tmp/presage-example", MigrationConflictStrategy::Drop, OnNewIdentity::Trust)?;
     ///
     ///     let (mut tx, mut rx) = oneshot::channel();
     ///     let (manager, err) = future::join(
