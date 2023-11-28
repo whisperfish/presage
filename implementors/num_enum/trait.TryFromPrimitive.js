5b6f6137333bc63aabc12be2b8b8c25aecb23845
gferon <g@leirbag.net>
2023-11-28 20:31:41 +0000

    Deploying to gh-pages from  @ 1bc98ac93f8c78b0e181b3656c16d6b45a1dcb54 🚀

--- implementors/num_enum/trait.TryFromPrimitive.js
+++ implementors/num_enum/trait.TryFromPrimitive.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"libsignal_protocol":[["impl TryFromPrimitive for <a class=\"enum\" href=\"libsignal_protocol/enum.ServiceIdKind.html\" title=\"enum libsignal_protocol::ServiceIdKind\">ServiceIdKind</a>"],["impl TryFromPrimitive for <a class=\"enum\" href=\"libsignal_protocol/enum.CiphertextMessageType.html\" title=\"enum libsignal_protocol::CiphertextMessageType\">CiphertextMessageType</a>"]]
+"libsignal_protocol":[["impl TryFromPrimitive for <a class=\"enum\" href=\"libsignal_protocol/enum.CiphertextMessageType.html\" title=\"enum libsignal_protocol::CiphertextMessageType\">CiphertextMessageType</a>"],["impl TryFromPrimitive for <a class=\"enum\" href=\"libsignal_protocol/enum.ServiceIdKind.html\" title=\"enum libsignal_protocol::ServiceIdKind\">ServiceIdKind</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
