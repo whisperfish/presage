81f751f122079cc80bf70b7f022e33063c99d6d3
gferon <g@leirbag.net>
2024-07-22 12:02:51 +0000

    Deploying to gh-pages from  @ b24845a5cb885fbab495b83ff4394427a4616702 🚀

---
+++ libsignal_service/proto/sync_message/delete_for_me/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["addressable_message","conversation_identifier"],"struct":["AddressableMessage","AttachmentDelete","ConversationDelete","ConversationIdentifier","LocalOnlyConversationDelete","MessageDeletes"]};
