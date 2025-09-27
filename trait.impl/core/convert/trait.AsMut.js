9d6f46d76fe2e1cd3593205b864c7d4c1c8a2295
gferon <g@leirbag.net>
2025-09-27 11:56:11 +0000

    Deploying to gh-pages from  @ 0357c532c5e539b09a4384a240a50a30fdc16f72 🚀

--- trait.impl/core/convert/trait.AsMut.js
+++ trait.impl/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["zkgroup",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;ShoHmacSha256&gt; for <a class=\"struct\" href=\"zkgroup/common/sho/struct.Sho.html\" title=\"struct zkgroup::common::sho::Sho\">Sho</a>"]]]]);
+    var implementors = Object.fromEntries([["zkgroup",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;ShoHmacSha256&gt; for <a class=\"struct\" href=\"zkgroup/common/sho/struct.Sho.html\" title=\"struct zkgroup::common::sho::Sho\">Sho</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
