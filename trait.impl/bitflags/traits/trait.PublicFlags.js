666bd1fb9857794ed8469c06bd0f3f22a8fd909c
gferon <g@leirbag.net>
2025-10-20 14:10:29 +0000

    Deploying to gh-pages from  @ ed011688fc8d9c0ee07c3d44743c138c1fa4dfda 🚀

---
+++ trait.impl/bitflags/traits/trait.PublicFlags.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libsignal_protocol",[["impl PublicFlags for <a class=\"struct\" href=\"libsignal_protocol/struct.SessionUsabilityRequirements.html\" title=\"struct libsignal_protocol::SessionUsabilityRequirements\">SessionUsabilityRequirements</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[237]}
---
+++ trait.impl/core/fmt/trait.Binary.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libsignal_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"libsignal_protocol/struct.SessionUsabilityRequirements.html\" title=\"struct libsignal_protocol::SessionUsabilityRequirements\">SessionUsabilityRequirements</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[358]}
