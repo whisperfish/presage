3dbeedbb088be9bcfe6c8658cb8c90fb169a9426
gferon <g@leirbag.net>
2025-12-12 13:50:05 +0000

    Deploying to gh-pages from  @ 66b56a7765cae7cfb83a610469562045902eb4c1 🚀

--- trait.impl/core/ops/bit/trait.BitAndAssign.js
+++ trait.impl/core/ops/bit/trait.BitAndAssign.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["libsignal_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"libsignal_protocol/struct.SessionUsabilityRequirements.html\" title=\"struct libsignal_protocol::SessionUsabilityRequirements\">SessionUsabilityRequirements</a>"]]]]);
+    var implementors = Object.fromEntries([["libsignal_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"libsignal_protocol/struct.SessionUsabilityRequirements.html\" title=\"struct libsignal_protocol::SessionUsabilityRequirements\">SessionUsabilityRequirements</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
