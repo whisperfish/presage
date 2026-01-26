097adba55b30d8627b40b1ca1c09344ef0063e2e
gferon <g@leirbag.net>
2026-01-26 09:57:49 +0000

    Deploying to gh-pages from  @ 907254d19f1524905d3e30275fa9e05f835ba9f1 🚀

--- trait.impl/core/fmt/trait.UpperHex.js
+++ trait.impl/core/fmt/trait.UpperHex.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["libsignal_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"libsignal_protocol/struct.SessionUsabilityRequirements.html\" title=\"struct libsignal_protocol::SessionUsabilityRequirements\">SessionUsabilityRequirements</a>"]]]]);
+    var implementors = Object.fromEntries([["libsignal_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"libsignal_protocol/struct.SessionUsabilityRequirements.html\" title=\"struct libsignal_protocol::SessionUsabilityRequirements\">SessionUsabilityRequirements</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
