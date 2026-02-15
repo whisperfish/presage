ead1ae4ffc92178a4edc9aa81854a38e8222cacc
gferon <g@leirbag.net>
2026-02-15 11:06:56 +0000

    Deploying to gh-pages from  @ 2064461ffcc694a911488e43b35f0aa0ffa0ca90 🚀

--- trait.impl/core/ops/arith/trait.SubAssign.js
+++ trait.impl/core/ops/arith/trait.SubAssign.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["libsignal_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a> for <a class=\"struct\" href=\"libsignal_protocol/struct.SessionUsabilityRequirements.html\" title=\"struct libsignal_protocol::SessionUsabilityRequirements\">SessionUsabilityRequirements</a>"]]]]);
+    var implementors = Object.fromEntries([["libsignal_protocol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a> for <a class=\"struct\" href=\"libsignal_protocol/struct.SessionUsabilityRequirements.html\" title=\"struct libsignal_protocol::SessionUsabilityRequirements\">SessionUsabilityRequirements</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
