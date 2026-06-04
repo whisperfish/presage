b7f255aca8dc9b3bbcb0a776c6885ffdc899ccc4
Schmiddiii <github@schmiddi.anonaddy.com>
2026-06-04 14:46:45 +0200

    Store receipt messages
    Before this change, it was impossible to e.g. have a window about who
    received/read a message (except for messages received while the app is
    running). By storing receipt messages, those can now be consumed by
    downstream apps when querying the store.

--- presage/src/manager/registered.rs
+++ presage/src/manager/registered.rs
@@ -1790,10 +1790,7 @@ async fn save_message<S: Store>(
             );
             None
         }
-        ContentBody::ReceiptMessage(msg) => {
-            debug!(?msg, "skipping saving receipt message");
-            None
-        }
+        ContentBody::ReceiptMessage(_) => Some(message),
         ContentBody::TypingMessage(msg) => {
             debug!(?msg, "skipping saving typing message");
             None
